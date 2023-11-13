8289814bd69630045b67df69bbd967632991247b
skogis02 <gskogsfors@gmail.com>
2023-11-13 12:18:00 +0100

    Update tramdata.py

--- tramdata.py
+++ tramdata.py
@@ -16,55 +16,95 @@ def build_tram_stops(jsonobject):
         print(stop_dictionary)
     
 
-def build_tram_lines(lines):
-    line_dictionary = {}
-    line_times = {}
-    stop_last = None
-    last_time = None
-    with open(lines, 'r') as linefile:
-        for line in linefile:
+def build_tram_lines(lines_file):
+    line_dict = {}
+    time_dict = {}
+
+    with open(lines_file, 'r', encoding='utf-8') as file:
+        current_line = None
+        current_line_stops = []
+        current_line_times = []
+
+        for line in file:
+            # Remove leading and trailing whitespaces
             line = line.strip()
-            if not line:
-                continue
-            elif line[-1] == ":":
-                current_line = line[:-1]
-                line_dictionary[current_line] = []
-            elif current_line:
-                stop_name = " ".join(line.split()[:-1])
-                line_dictionary[current_line].append(stop_name)
-                stop_time_str = line.split()[:-1]
-                stop_time = int(stop_time_str.split(':')[1])
-                
-                if stop_last:
-                    transport_time = (stop_last - last_time)%60
-                    if stop_last not in line_times:
-                        line_times[stop_last] = {}
-                    line_times[stop_last][stop_name]=transport_time
-       
-    desired_key = '2'
-    print(line_dictionary[desired_key])
-        
-        
 
-        
+            if line.endswith(':'):
+                # Save the previous line information if any
+                if current_line is not None:
+                    line_dict[current_line] = current_line_stops
 
+                    current_line_stops = []
+                    current_line_times = []
+                # Set the current line
+                current_line = line[:-1]  # Remove the colon
+            else:
+                stop_time_parts = line.rsplit(' ', 1)
 
+                if len(stop_time_parts) == 2:
+                    stop = stop_time_parts[0].rstrip()
+                    time = stop_time_parts[1]
+
+                    minutes = int(time.split(':')[1])
+
+                    current_line_stops.append(stop)
+                    current_line_times.append(minutes)
+
+                # Add stop and transition time to time_dict
+                    if stop not in time_dict:
+                        time_dict[stop] = {}
+
+                    for i in range(len(current_line_stops) - 1):
+                        source_stop = current_line_stops[i]
+                        dest_stop = current_line_stops[i + 1]
+                        transition_time = current_line_times[i+1] - current_line_times[i]
+                        time_dict[source_stop][dest_stop] = transition_time
+            
+            if current_line is not None: 
+                line_dict[current_line] = current_line_stops
+    return line_dict, time_dict
+    
 
 def build_tram_network(stopfile, linefile):
-    ## YOUR CODE HERE
-    pass
+    tram_network = {
+        "stops": build_tram_stops(stopfile),
+        "lines": build_tram_lines(linefile)[0],
+        "times": build_tram_lines(linefile)[1]
+    }
+    return tram_network
 
-def lines_via_stop(linedict, stop):
-    ## YOUR CODE HERE
-    pass
+
+def lines_via_stop(linedict, stop): #tar in min linedictionary {'1': ['Östra Sjukhuset', 'Tingvallsvägen', 'Kaggeledstorget'
+    lines_via = []
+
+    for line, stops in linedict.items():
+        if any(stop.lower() == s.lower() for s in stops):
+            lines_via.append(line)
+    return lines_via
+
 
 def lines_between_stops(linedict, stop1, stop2):
-    ## YOUR CODE HERE
-    pass
+    lines_between = []
+
+    for line, stops in linedict.items(): 
+        if any(stop1.lower() == s.lower() for s in stops) and any(stop2.lower() == d.lower() for d in stops): 
+            lines_between.append(line)
+    return lines_between
 
 def time_between_stops(linedict, timedict, line, stop1, stop2):
-    ## YOUR CODE HERE
-    pass
+    time_between = 0
+    stops = linedict[line] #Plockar ut listan med stops från line#
+    if any(stop1.lower() == s.lower() for s in stops) and any(stop2.lower() == d.lower() for d in stops): 
+        if stops.index(stop1) < stops.index(stop2):
+            return str('rätt ordning')
+        else: 
+            stop1_1 = stop2
+            stop2_2 =  stop1
+            print(stop1_1, stop2_2)
+            return str('klara är dum')
+    else:
+        return str('False')
+        
 
 def distance_between_stops(stopdict, stop1, stop2):
     ## YOUR CODE HERE
@@ -85,4 +125,7 @@ if __name__ == '__main__':
         dialogue()
      
 
-build_tram_lines(LINE_FILE)
+#print(build_tram_lines(LINE_FILE))
+#print(build_tram_network(STOP_FILE, LINE_FILE))
+#print(lines_via_stop(build_tram_lines(LINE_FILE)[0],'Östra Sjukhuset'))
+print(time_between_stops(build_tram_lines(LINE_FILE)[0],build_tram_lines(LINE_FILE)[1],'6','Chalmers','Korsvägen'))
